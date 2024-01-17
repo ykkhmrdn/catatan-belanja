6ba8b3a7b86a561a9db7fc7e18815272e00a7a65
ykkhmrdn <ykkhmrdn@gmail.com>
2024-01-18 02:21:24 +0700

    Add homepage and deployment scripts
    This commit adds a homepage URL to the package.json file and includes predeploy and deploy scripts for deployment. The vite.config.js file is also updated to specify the base URL and build options.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/catatan-belanja/",
+  build: {
+    outDir: "dist",
+    assetsDir: "./", // add this line
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+});
